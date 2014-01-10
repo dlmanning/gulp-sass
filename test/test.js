1ee0a2ac6ccefffe33bf5c26c6fe67111435e1e3
David Manning <dlmanning@me.com>
2014-01-10 01:45:46 -0800

    Added tape tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,27 @@
+var path = require('path');
+var fs = require('fs');
+
+var test = require('tape');
+
 var gulp = require('gulp')
-  , sass = require('../')
+  , util = require('gulp-util')
+  , gulpSass = require('../')
+  , nodeSass = require('node-sass');
   ;
 
 gulp.src('./scss/*.scss')
-  .pipe(sass())
-  .pipe(gulp.dest('./css/'));
+    .pipe(gulpSass())
+    .pipe(util.buffer(tapeTester));
+
+function tapeTester(err, files) {
+  files.forEach(function (item) {
+    var fileBaseName = path.basename(item.path, '.css');
+    test('Testing: ' + fileBaseName, function (t) {
+      var ref = nodeSass.renderSync({
+        data: fs.readFileSync('./scss/' + fileBaseName + '.scss').toString()
+      })
+      t.equal(item.contents.toString(), ref);
+      t.end();
+    })
+  });
+}
