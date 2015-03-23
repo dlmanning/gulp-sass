1dd861e4d6e0daa560c085a75dfdf6344954b814
Vincent Prouillet <vincent@wearewizards.io>
2015-03-23 22:01:12 +0000

    Rewrite gulp-sass to be a very light wrapper
    Simply pass everything to node-sass and just hand
    back the results to gulp.
    Removed custom options like success/failure cb
    and sync rendering.
    
    Missing:
     -  gulp-sourcemaps support + test

--- index.js
+++ index.js
@@ -1,104 +1,38 @@
-var fs    = require('fs')
-  , map   = require('map-stream')
-  , nodeSass  = require('node-sass')
-  , path  = require('path')
-  , gutil = require('gulp-util')
-  , clone = require('clone')
-  , ext   = gutil.replaceExtension
-  , applySourceMap = require('vinyl-sourcemaps-apply')
-  ;
+var gutil = require("gulp-util");
+var through = require("through2");
+var assign = require("object-assign");
+var path  = require('path');
+var sass = require("node-sass");
 
-module.exports = function (options) {
+var PLUGIN_NAME = 'gulp-sass';
 
-  function sass (file, cb) {
-    var opts = options ? clone(options) : {};
-    var fileDir = path.dirname(file.path);
-
+module.exports = function(options) {
+  return through.obj(function(file, enc, cb) {
     if (file.isNull()) {
       return cb(null, file);
+    }
+    if (file.isStream()) {
+      return cb(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
     }
     if (path.basename(file.path).indexOf('_') === 0) {
       return cb();
     }
 
-    if (file.sourceMap) {
-      opts.sourceMap = file.path;
-    }
-
-    opts.data = file.contents.toString();
+    var opts = assign({}, options);
     opts.file = file.path;
 
-    if (opts.includePaths && Array.isArray(opts.includePaths)) {
-      if (opts.includePaths.indexOf(fileDir) === -1) {
-        opts.includePaths.push(fileDir);
-      }
-    } else {
-      opts.includePaths = [fileDir];
-    }
-
-    opts.success = function (obj) {
-      if (typeof opts.onSuccess === 'function') opts.onSuccess(obj);
-
-      if (obj.map && typeof obj.map === 'string') {
-        // hack to remove the already added sourceMappingURL from libsass
-        obj.css = obj.css.replace(/\/\*#\s*sourceMappingURL\=.*\*\//, '');
-
-        // libsass gives us sources' paths relative to file;
-        // gulp-sourcemaps needs sources' paths relative to file.base;
-        // so alter the sources' paths to please gulp-sourcemaps.
-        obj.map = JSON.parse(obj.map);
-
-        if (obj.map.sources) {
-          obj.map.sources = obj.map.sources.map(function(source) {
-            var abs = path.resolve(path.dirname(file.path), source);
-            return path.relative(file.base, abs);
-          });
-
-          obj.map = JSON.stringify(obj.map);
-          applySourceMap(file, obj.map);
-        }
-
+    var callback = function(error, obj) {
+      if (error) {
+        return cb(new gutil.PluginError(
+          PLUGIN_NAME, error.message + ' on line ' + error.line + ' in ' + error.file
+        ));
       }
 
-      handleOutput(obj, file, cb);
+      file.contents = new Buffer(obj.css);
+      file.path = gutil.replaceExtension(file.path, '.css');
+      cb(null, file);
     };
 
-    opts.error = function (err) {
-      if (opts.errLogToConsole) {
-        gutil.log(gutil.colors.red('[gulp-sass]', err.message, 'on line', err.line + 'in', err.file));
-        return cb();
-      }
-
-      if (typeof opts.onError === 'function') {
-        opts.onError(err);
-        return cb();
-      }
-      
-      err.lineNumber = err.line;
-      err.fileName = err.file;
-
-      return cb(new gutil.PluginError('gulp-sass', err));
-    };
-
-    if ( opts.sync ) {
-      try {
-        var output = nodeSass.renderSync(opts);
-        opts.success(output);
-        handleOutput(output, file, cb);
-      } catch(err) {
-        opts.error(err);
-      }
-    } else {
-      nodeSass.render(opts);
-    }
-
-  }
-
-  return map(sass);
+    sass.render(opts, callback);
+  });
 };
-
-function handleOutput(output, file, cb) {
-  file.path = ext(file.path, '.css');
-  file.contents = new Buffer(output.css);
-  cb(null, file);
-}
